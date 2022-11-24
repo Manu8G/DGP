d3eacea74884d81107e250780fe4cbe3426d2567
Manu8G-WindowsZenbook <manuel8guerrero@hotmail.com>
2022-11-24 11:31:08 +0100

    Primer video terminado pero no ok

--- app/tecnogurus/views.py
+++ app/tecnogurus/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .models import Profile, Friend
 
 def index(request):
-    context = {}
+    user = request.user.profile
+    friends = user.friends.all()
+    context = {"user": user, "friends": friends}
     return render(request, 'tecnogurus/index.html', context)
 
-def detail(request):
-    context = {}
+def detail(request,pk):
+    friend = Friend.object.get(profile_id=pk)
+    context = {"friend": friend}
     return render(request, "tecnogurus/detail.html", context)
