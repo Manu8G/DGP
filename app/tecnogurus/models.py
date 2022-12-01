9011aef28faa21e5d7f1aaefa3b2afcdd67f43e5
luistf24 <luis.tormo24@gmail.com>
2022-12-01 19:53:13 +0100

    Añado que se actualice la página al borrar

--- app/tecnogurus/models.py
+++ app/tecnogurus/models.py
@@ -2,13 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Usuario(models.Model):
-    usuario     = models.CharField(max_length = 10)
-    password    = models.CharField(max_length = 30)
-
-    def __str__(self):
-        return self.usuario
-
 class Curso(models.Model):
     id_curso    = models.CharField(max_length = 10)
 
--- app/tecnogurus/views.py
+++ app/tecnogurus/views.py
@@ -1,7 +1,7 @@
 from cgi import print_arguments
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from .models import Alumno, Curso, Usuario, Tarea
+from .models import Alumno, Curso
 import json
 
 def index(request):
@@ -21,70 +21,5 @@ def chat_rooms(request):
 
     return render(request, "chat_rooms.html", {"curso": curso}) 
 
-
 def room(request, room_name):
     return render(request, "room.html", {"room_name": room_name})
-
-
-def login_picto(request):
-    contra = Usuario.password
-    user = Usuario.objects.all()
-    users = Usuario.objects.all().count
-    
-    return render(request, 'login_picto.html', context={'contra':contra, 'user':user, 'users':users})
-
-
-def request_login(request):
-   return render(request, 'request_login.html')
-
-
-def buscador(request):
-    return render(request, 'buscador.html')
-
-
-def login_ok(request):
-    return render(request, 'login_ok.html')
-
-
-def failed_login(request):
-    return render(request, 'failed_login.html')
-
-
-def prueba(request):
-    if request.method == 'POST':
-        arr = request.POST.getlist('arr[]')
-        us = arr[0]
-        prueba = ''
-        cont = ''
-        cont += arr[1]
-        cont += arr[2]
-        cont += arr[3]
-        cont += arr[4]
-        guardar = 'nok'
-
-        user = Usuario.objects.all()
-        for u in user:
-            if u.usuario == us:
-                prueba = 'perfect'
-                if u.password == cont:
-                    prueba = 'perfectx2'
-                    JsonResponse(prueba, safe=False)
-                
-            else:
-                prueba = 'fallo'
-
-        
-            
-        return JsonResponse(prueba, safe=False)
-    else:
-        return JsonResponse(guardar, safe=False)
-
-
-def fin_tarea(request):
-    num_tareas = Tarea.objects.all().count
-    id_t = Tarea.id_tarea
-    tarea_p = Tarea.objects.all()
-    return render(
-        request, 'fin_tarea.html', 
-        context={'num_tareas':num_tareas, 'id_t':id_t, 'tarea_p':tarea_p}
-    )
